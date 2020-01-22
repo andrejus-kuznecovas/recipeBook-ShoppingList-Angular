0e31ec8471195e588dc7fa1cf0e2e66bf1504f21
Andrej Kuznecov <aku@tiatechnology.com>
2020-01-22 12:26:41 +0200

    Remove unnecessary code

--- recipeShop/src/app/recipes/recipes.component.ts
+++ recipeShop/src/app/recipes/recipes.component.ts
@@ -7,13 +7,7 @@ import { Recipe } from "./recipe.model";
   styleUrls: ["./recipes.component.css"]
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      "Test recipe",
-      "Test description",
-      "https://agro-market24.eu/media/cache/ad_photo_main/uploads/photos/30838/sprzedam_owoce-%C5%9Bwie%C5%BCe-kiwi-_agro%20market24_og%C5%82oszenia%20rolnicze-870336-30838.jpg"
-    )
-  ];
+
   constructor() {}
 
   ngOnInit() {}
